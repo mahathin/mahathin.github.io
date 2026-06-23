fb2029c607d4af2d7718f10044c8de27a4788897
Mahathi Narayanaswamy <114795724+mahathin@users.noreply.github.com>
2026-06-23 14:55:47 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 // for Navigation, animations, interactions
+
+
+// Mobile nav toggle
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('main-nav');
+
+if (hamburger && nav) {
+  hamburger.addEventListener('click', () => {
+    nav.querySelector('ul').classList.toggle('open');
+  });
+}
